f8d191d7920d102aa99de72e58ec9ae12c38582e
三井田智浩 <tomohiro@Tomo-Mac.local>
2016-01-17 17:53:35 +0900

    argv practice

---
+++ argv_practice.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == "__main__":
+
+    pram = sys.argv
+    print pram
+
+print u"No1 arg:" + pram[1]
+print u"No2 arg:" + pram[2]
+print u"No3 arg:" + pram[3]
