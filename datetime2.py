7273011c8f5e85bbc20eb8df58f8d9779930286c
tomopiron2015 <chump.2006@gmail.com>
2016-01-25 17:40:13 +0900

    Update datetime2.py

--- datetime2.py
+++ datetime2.py
@@ -3,21 +3,10 @@
 import datetime
 
 if __name__ == "__main__":
-
-    today = datetime.today()
-
-    #    今日の日付
+    today    =    datetime.datetime.today()
     print today
-
-    #    明日の日付
     print today + datetime.timedelta(days=1)
-
     newyear    =    datetime.datetime(2010,1,1)
-
-    #    2010年1月1日の一週間後
     print newyear + datetime.timedelta(days=7)
-
-    #    2010年1月1日から今日までの日数
     calc    =    today - newyear
-    #    計算結果の返り値は「timedelta」
     print calc.days
